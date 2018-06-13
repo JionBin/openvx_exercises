d8ae31ab4b03981f311779cd0c5e79cc405d6c99
Devin Jiao <bin.jiao@nxp.com>
2018-06-13 09:15:54 +0800

    Get openvx version

--- get_openvx_version/openvx_version.cpp
+++ get_openvx_version/openvx_version.cpp
@@ -4,19 +4,18 @@
 
 int main()
 {
-	vx_uint16 num;
-	vx_uint16 vendor;
-	vx_context context;
-	vx_char imp[VX_MAX_IMPLEMENTATION_NAME];
-	
-	context = vxCreateContext();
+  vx_uint16 num;
+  vx_uint16 vendor;
+  vx_context context;
+  vx_char imp[VX_MAX_IMPLEMENTATION_NAME];
 
-  vxQueryContext(context, VX_CONTEXT_VERSION, &num, sizeof(num));
+  context = vxCreateContext();
+
+	vxQueryContext(context, VX_CONTEXT_VERSION, &num, sizeof(num));
   vxQueryContext(context, VX_CONTEXT_IMPLEMENTATION, imp, sizeof(imp));
-  
+
 	printf("Implementation = %s\n",imp);
 	printf("Version = %x\n", num);
-
-	vxReleaseContext(&context);
-	return 0;
+  vxReleaseContext(&context);
+  return 0;
 }
